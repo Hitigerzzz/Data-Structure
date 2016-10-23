97d3c44b235638ba7e1a105335c5d88b51da1a2f
JasonCarryU <734779745@qq.com>
2016-10-23 22:29:35 +0800

    .

--- Josephus.java
+++ Josephus.java
@@ -2,12 +2,11 @@
 
 public class Josephus {
 	public static void main(String[] args) {
-	    int n = 7, m = 3;
-	     
+	    int n = 500, m = 3;
 	    int answer = 0;  
-        for(int i = 1; i <= n; i++) {  
+        for(int i = 2; i <= n; i++) {  
             answer = (answer + m) % i;  
-            System.out.println("Survival: " + answer);  
         }  
+        System.out.println("Survival: " + (answer+1)); 
 	}
 }
